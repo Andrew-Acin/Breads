c925a075b84e7eab5cc87a089b7cfe917c469d25
Andrew Acin <andrew_acin@hotmail.com>
2024-05-06 21:44:20 -0700

    create new routes complete

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }))
+
 
 
 
@@ -19,16 +22,16 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
-// 404 Page
-app.get('*', (req, res) => {
-    res.send('404')
-})
-
-
 
 // Breads
 const breadsController = require('./controllers/breads_controllers')
 app.use('/breads', breadsController)
+
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+})
 
 
 // Listen
