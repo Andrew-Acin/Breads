7aa961991d1bc5081bd785d17de9f51a06d735ba
Andrew Acin <andrew_acin@hotmail.com>
2024-05-06 19:15:13 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require ('express')
 
+// Config
 require('dotenv').config()
 const PORT = process.env.PORT
 console.log(PORT)
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
+
+
 // Routes
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+})
+
 
 
 // Breads
