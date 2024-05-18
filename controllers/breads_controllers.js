b70bdce2082378df1958331f7fb271b0889731df
Andrew Acin <andrew_acin@hotmail.com>
2024-05-18 11:35:36 -0700

    BreadCRUD Populate

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -28,19 +28,20 @@ breads.get('/new', (req, res) => {
 })
 
 
-
 // SHOW
 breads.get('/:id', (req, res) => {
     Bread.findById(req.params.id)
+        .populate('baker')
         .then(foundBread => {
-            const bakedBy = foundBread.getBakedBy()
-            console.log(bakedBy)
             res.render('show', {
                 bread: foundBread
             })
         })
-        
+        .catch(err => {
+            res.send('404')
+        })
 })
+
 
 
 
@@ -89,11 +90,16 @@ breads.put('/:id/edit', (req, res) => {
 
 // EDIT
 breads.get('/:id/edit', (req, res) => {
-    Bread.findById(req.params.id)
-        .then(foundBread => {
-            res.render('edit', {
-                bread: foundBread
-            })
+    Baker.find()
+        .then(foundBakers => {
+            Bread.findById(req.params.id)
+                .then(foundBread => {
+                    res.render('edit', {
+                        bread: foundBread,
+                        bakers: foundBakers
+                    })
+                })
+    
         })
 })
 
